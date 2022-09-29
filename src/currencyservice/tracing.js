3c0d064d0c1b33f9178c6656aa0011f0a6ab35f7
Bahubali Shetti <bahubalishetti@bahubalis-mbp.lan>
2022-09-29 10:51:58 -0700

    modified nodeJs files for grpc in tracer.js file

--- src/currencyservice/tracing.js
+++ src/currencyservice/tracing.js
@@ -1,9 +1,18 @@
 const opentelemetry = require("@opentelemetry/sdk-node");
 const { getNodeAutoInstrumentations } = require("@opentelemetry/auto-instrumentations-node");
 const { OTLPTraceExporter } =  require('@opentelemetry/exporter-trace-otlp-grpc');
+const grpc = require('@grpc/grpc-js');
+const { diag, DiagConsoleLogger, DiagLogLevel } = require('@opentelemetry/api');
+
+// For troubleshooting, set the log level to DiagLogLevel.DEBUG
+diag.setLogger(new DiagConsoleLogger(), DiagLogLevel.INFO);
+
+const collectorOptions = {
+  credentials: grpc.credentials.createSsl()
+};
 
 const sdk = new opentelemetry.NodeSDK({
-  traceExporter: new OTLPTraceExporter(),
+  traceExporter: new OTLPTraceExporter(collectorOptions),
   instrumentations: [getNodeAutoInstrumentations()]
 });
 
